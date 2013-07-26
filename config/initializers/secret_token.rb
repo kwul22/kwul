d8a564b995addaa07cf05bebdd6c70803ffffff2
Kendall Wu <kendall.wu@gmail.com>
2013-07-25 18:07:28 -0700

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Kwul::Application.config.secret_token = '7d64906cff890a4ede3cd75ea8cdad18fc7ce0b06352185e692ba23300aab589c12e5b3679c00073d94dd3b481a2195a032b4254cf473d80d982c0009fedb6ef'
